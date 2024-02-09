bcf330d9e1845b1cb0c5c298d092a078c6228f59
Prashanth Dussa <prashanth.dussa05@gmail.com>
2024-02-09 15:56:04 -0700

    modified

--- src/Die.js
+++ src/Die.js
@@ -1,12 +1,17 @@
 import React from "react";
-
+import Dot from './Dot'
 export default function Die(props){
     const styles={
         backgroundColor: props.isHeld? "#59E391":"white"
     }
+    const dotsArray=Array.from({ length: props.value }, (_, index) => index + 1)
+    const dots= dotsArray.map(()=><Dot/>)
     return(
         <div className="die-face" style={styles} onClick={props.holdDice}>
-            <h2>{props.value}</h2>
+            {/* <h2>{props.value}</h2> */}
+            <div className="dots">
+                {dots}
+            </div>
         </div>
     )
 }
